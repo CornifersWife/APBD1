f8d0ede5aed1096572eef01478b185049cc6cc14
CornifersWife <irys.latosz@gmail.com>
2024-03-11 13:17:45 +0100

    added AvgValuemethod

--- Zajecia1/Zajecia1/Program.cs
+++ Zajecia1/Zajecia1/Program.cs
@@ -2,8 +2,18 @@
 
 public class Program {
     public static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World! s");
         Console.WriteLine("Do commita 2");
         Console.WriteLine("Do commita 3");
+    }
+
+    public static float AvgValue(int[] numbers) {
+        float avgValue = 0f;
+        for (int i = 0; i < numbers.Length; i++) {
+            avgValue += numbers[i];
+        }
+
+        return (avgValue / numbers.Length);
+        
     }
 }
